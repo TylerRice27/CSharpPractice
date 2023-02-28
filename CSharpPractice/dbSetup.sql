dd22b19e529dfa4f001d5e1c7f3cc33e01d26b24
TylerRice27 <Tylerrayrice2@gmail.com>
2023-02-28 15:04:24 -0700

    Tables made

--- CSharpPractice/dbSetup.sql
+++ CSharpPractice/dbSetup.sql
@@ -1,8 +1,39 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS cities(
+        id INT NOT NULL AUTO_INCREMENT primary key COMMENT "city id primary key",
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Updated',
+        name VARCHAR(255) COMMENT 'City Name'
+    ) DEFAULT charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS teams(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Team Id primary key",
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Updated',
+        name VARCHAR(500) COMMENT 'Team Name'
+    ) DEFAULT charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS heros(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY key COMMENT "Hero Id Primary Key",
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Time Created",
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Updated',
+        name VARCHAR(500) COMMENT 'Hero Name',
+        img VARCHAR(2000) COMMENT 'Hero Image',
+        bio VARCHAR(500) COMMENT 'Hero Bio',
+        cityId int NOT NULL,
+        teamId int,
+        Foreign Key (cityId) REFERENCES cities(id) ON DELETE CASCADE,
+        Foreign Key (teamId) REFERENCES teams(id) ON DELETE CASCADE
+    ) DEFAULT charset utf8 COMMENT '';
